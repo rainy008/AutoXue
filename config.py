0f2e32cb7c0fcb5827feb050afe5856e8e626d28
kessil <kessil@outlook.com>
2019-06-15 07:55:09 +0800

    添加自动答题功能

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     XML_URI = os.path.join(basedir, 'ui.xml')
     DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    DATABASE_URI2 = 'sqlite:///' + os.path.join(basedir, 'data-prod.sqlite')
+    REPEAT_TIMES = 1
     # 解析xml的xpath规则，针对不同xml规则不同
     XPATH_QUESTION = '//node[@class="android.webkit.WebView"]/node/node[@index="2"]/node/node[@index="1"]/node[@index="0"]'
     XPATH_CONTENT = './node[@index="0"]/@content-desc'
